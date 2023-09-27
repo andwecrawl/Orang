810aa18ac174aa18123376cd5b692dc2f319ce63
yeoni <ghj0308@daum.net>
2023-09-27 13:43:01 +0900

    feat: ReusableViewProtocol 추가
    identifier 구성

---
+++ Orang/Orang/Scene/Base/Protocol/reusableViewProtocol.swift
@@ -0,0 +1,25 @@
+//
+//  reusableViewProtocol.swift
+//  Orang
+//
+//  Created by yeoni on 2023/09/27.
+//
+
+import UIKit
+
+protocol ReusableViewProtocol: AnyObject {
+    static var identifier: String { get }
+}
+
+extension UIViewController: ReusableViewProtocol {
+    static var identifier: String {
+        return self.description()
+    }
+}
+
+extension UICollectionViewCell: ReusableViewProtocol {
+    static var identifier: String {
+        return self.description()
+    }
+}
+
