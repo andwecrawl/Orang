f7d63208fe1e13ed38229aca5f0b1e8d5c7de28f
yeoni <ghj0308@daum.net>
2023-09-28 12:31:00 +0000

    feat: baseTableViewCell 생성

---
+++ Orang/Orang/Scene/Base/BaseTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  BaseTableViewCell.swift
+//  Orang
+//
+//  Created by yeoni on 2023/09/27.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureView()
+        setConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+    
+    func configureView() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+    
+}
