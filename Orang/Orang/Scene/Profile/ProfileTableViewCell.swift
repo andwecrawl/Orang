32e1b320706f908154dac348dc76c5ff991aacf5
yeoni <ghj0308@daum.net>
2023-09-29 19:04:37 +0900

    feat: ProfileVC tableView 추가 후 연결
    작동 확인도 함

---
+++ Orang/Orang/Scene/Profile/ProfileTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileTableViewCell.swift
+//  Orang
+//
+//  Created by yeoni on 2023/09/27.
+//
+
+import UIKit
+
+class ProfileTableViewCell: BaseTableViewCell {
+    
+    
+    
+    override func configureView() {
+//        self.contentView.backgroundColor = .red
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+}
--- Orang/Orang/Scene/Profile/ProfileViewController.swift
+++ Orang/Orang/Scene/Profile/ProfileViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ProfileViewController: BaseViewController {
 
+    let tableView = {
+        let view = UITableView(frame: .zero)
+        view.register(ProfileTableViewCell.self, forCellReuseIdentifier: ProfileTableViewCell.identifier)
+        view.showsVerticalScrollIndicator = false
+        view.separatorStyle = .none
+        view.rowHeight = 180
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +29,8 @@ class ProfileViewController: BaseViewController {
         
         title = "Profile"
         view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     func setNavigationBar() {
@@ -38,8 +48,30 @@ class ProfileViewController: BaseViewController {
     }
 
     override func setConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
 
+extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ProfileTableViewCell.identifier) as? ProfileTableViewCell else { return UITableViewCell() }
+        cell.backgroundColor = .cyan
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("clicked!")
+    }
+    
+//    func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+//        
+//    }
     
 }
