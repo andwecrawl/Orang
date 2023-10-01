54d17c9fc71610aeec064ede381f067a735b1f67
yeoni <ghj0308@daum.net>
2023-10-01 16:44:31 +0900

    feat: Localization Setting

---
+++ Orang/Orang/Localization/Extension/String+.swift
@@ -0,0 +1,24 @@
+//
+//  String+.swift
+//  Orang
+//
+//  Created by yeoni on 2023/09/30.
+//
+
+import Foundation
+
+extension String {
+    
+    func localized(comment: String = "") -> String {
+        return NSLocalizedString(self, comment: comment)
+    }
+    func localized(with argument: CVarArg = [], comment: String = "") -> String {
+        return String(format: self.localized(comment: comment), argument)
+    }
+}
+
+/*
+ ~ 사용 예시 ~
+ myLabel.text = "Hello".localized()
+ myLabel.text = "My Age %d".localized(with: 26, comment: "age")
+ */
