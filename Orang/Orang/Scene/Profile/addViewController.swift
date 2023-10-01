e6ef44c77a9e44d950af2ace6046a1a6b8d5cf49
yeoni <ghj0308@daum.net>
2023-10-01 16:44:45 +0900

    feat: AddViewController

---
+++ Orang/Orang/Scene/Profile/addViewController.swift
@@ -0,0 +1,27 @@
+//
+//  addViewController.swift
+//  Orang
+//
+//  Created by yeoni on 2023/09/30.
+//
+
+import UIKit
+
+class addViewController: BaseViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+}
