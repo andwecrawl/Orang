265b51b652bd3c45420cfeb273e8893e1a7fb25d
yeoni <ghj0308@daum.net>
2023-10-02 17:45:05 +0900

    feat: 프로필, 이름, 생일 추가

--- Orang/Orang/Scene/Profile/addViewController.swift
+++ Orang/Orang/Scene/Profile/addViewController.swift
@@ -9,7 +9,16 @@ import UIKit
 
 class addViewController: BaseViewController {
     
+    let profileImageView = UIImageView.imageViewBuilder(size: 130)
+    let profileImageButton = UIButton.profileButtonBuilder(size: 130)
     
+    let nameLabel = UILabel.labelBuilder(text: "name".localized(), size: 16, weight: .bold)
+    let nameTextField = UnderLineTextField.textFieldBuilder(placeholder: "inputName".localized())
+    let nameStackView = UIStackView.stackViewBuilder()
+    
+    let birthLabel = UILabel.labelBuilder(text: "birthday".localized(), size: 16, weight: .bold)
+    let birthTextField = UnderLineTextField.textFieldBuilder(placeholder: "inputBirthday".localized())
+    let birthStackView = UIStackView.stackViewBuilder()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +31,48 @@ class addViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
+        super.configureHierarchy()
         
+        view.addSubview(profileImageView)
+        view.addSubview(profileImageButton)
+        
+        nameStackView.AddArrangedSubviews([nameLabel, nameTextField])
+        birthStackView.AddArrangedSubviews([birthLabel, birthTextField])
     }
     
     override func setConstraints() {
+        profileImageView.backgroundColor = .gray
+        profileImageView.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
         
+        profileImageButton.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView)
+            make.centerX.equalTo(profileImageView)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.width.equalTo(80)
+            make.height.equalTo(50)
+        }
+        nameStackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.top.equalTo(profileImageView.snp.bottom).offset(30)
+            make.height.equalTo(50)
+        }
+        setStackViewConstrations(stackView: birthStackView, label: birthLabel, topElement: nameStackView)
+    func setStackViewConstrations(stackView: UIStackView, label: UILabel, topElement: UIView) {
+        label.snp.makeConstraints { make in
+            make.width.equalTo(80)
+            make.height.equalTo(50)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.top.equalTo(topElement.snp.bottom).offset(16)
+            make.height.equalTo(50)
+        }
     }
     
 }
