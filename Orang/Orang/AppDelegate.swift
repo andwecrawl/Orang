cc7abbb9b18d94e92009d300ff6e354730ae98f5
yeoni <ghj0308@daum.net>
2023-10-02 21:58:15 +0900

    edit: IQKeyboardManager 설정 변경

--- Orang/Orang/AppDelegate.swift
+++ Orang/Orang/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.overrideKeyboardAppearance = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         UINavigationBar.appearance().tintColor = Design.Color.tintColor
         UITabBar.appearance().tintColor = Design.Color.tintColor
         
