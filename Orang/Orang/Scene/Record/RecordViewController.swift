992bb7223f0d88cff4067a285518e81de0c27409
yeoni <ghj0308@daum.net>
2023-10-03 15:55:33 +0900

    feat: RecordVC

---
+++ Orang/Orang/Scene/Record/RecordViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RecordViewController.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/03.
+//
+
+import UIKit
+
+class RecordViewController: BaseViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+}
