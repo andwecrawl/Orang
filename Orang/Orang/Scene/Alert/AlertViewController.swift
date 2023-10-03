24faa3d60743c3af54d07a67b7f232abb58c6eac
yeoni <ghj0308@daum.net>
2023-10-03 15:55:48 +0900

    feat: AlertVC

---
+++ Orang/Orang/Scene/Alert/AlertViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AlertViewController.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/03.
+//
+
+import UIKit
+
+class AlertViewController: BaseViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+}
