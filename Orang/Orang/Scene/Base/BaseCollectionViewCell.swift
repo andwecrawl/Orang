ccc64be8df1014faf795733cd497483dc4b2f481
yeoni <ghj0308@daum.net>
2023-10-05 14:10:59 +0900

    feat: BaseCollectionViewCell

---
+++ Orang/Orang/Scene/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  BaseCollectionViewCell.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/05.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        setConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+    
+}
