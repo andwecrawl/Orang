0c6ebfd1afd664d45d215ddb64e49e2b260888bb
yeoni <ghj0308@daum.net>
2023-10-07 11:57:14 +0900

    feat: 붙여넣기 막는 함수 추가

--- Orang/Orang/Scene/Base/Builder/UnderlineTextFieldBuilder.swift
+++ Orang/Orang/Scene/Base/Builder/UnderlineTextFieldBuilder.swift
@@ -95,3 +95,13 @@ extension UnderLineTextField {
         return textField
     }
 }
+
+extension UnderLineTextField {
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        
+        if action == #selector(paste(_:)) {
+            return false
+        }
+        return super.canPerformAction(action, withSender: sender)
+    }
+}
