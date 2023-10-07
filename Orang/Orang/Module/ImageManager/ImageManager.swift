f9769033b1bc627dd30d67f527a68a30818476ef
yeoni <ghj0308@daum.net>
2023-10-07 16:49:58 +0900

    edit: fileManager 설정 후 간단하게 리팩토링함

--- Orang/Orang/Module/ImageManager/ImageManager.swift
+++ Orang/Orang/Module/ImageManager/ImageManager.swift
@@ -19,10 +19,30 @@ class ImageManager {
     
     let fileManager = FileManager.default
     
-    func makeDirectory(directoryName: DirectoryName) {
-        
+    
+    enum FileURLCase {
+        case directory, fileURL
+    }
+    
+    func createURL(URLCase: FileURLCase, directoryName: DirectoryName, identifier: String) -> (URL, String) {
         let documentURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
         let directoryURL = documentURL.appendingPathComponent(directoryName.rawValue)
+        var imageName = "\(directoryName.rawValue)_\(identifier).jpg"
+        let fileURL = directoryURL.appendingPathComponent(imageName)
+        switch URLCase {
+        case .directory:
+            return (directoryURL, imageName)
+        case .fileURL:
+            return (fileURL, imageName)
+        }
+    }
+    
+    
+    func makeDirectory(directoryName: DirectoryName) {
+        
+        let (directoryURL, _) = createURL(URLCase: .directory, directoryName: directoryName, identifier: "")
+        
+        if fileManager.fileExists(atPath: directoryURL.path) { return }
         
         do {
             try fileManager.createDirectory(at: directoryURL, withIntermediateDirectories: false, attributes: nil)
@@ -31,45 +51,56 @@ class ImageManager {
         }
     }
     
-    func createFileURL(directoryName: DirectoryName, identifier: String) -> URL {
-        let documentURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let directoryURL = documentURL.appendingPathComponent(directoryName.rawValue)
-        var imageName = "\(directoryName.rawValue)_\(identifier)"
-        let fileURL = directoryURL.appendingPathComponent(identifier, conformingTo: .jpeg)
-        return fileURL
+    func saveImageToDirectory(directoryName: DirectoryName, identifier: String, image: UIImage?) -> Bool {
+        let (fileURL, imageName) = createURL(URLCase: .fileURL, directoryName: directoryName, identifier: identifier)
+        guard let image else { return false }
+        
+        do {
+            if let imageData = image.jpegData(compressionQuality: 0.3) {
+                try imageData.write(to: fileURL)
+            }
+            return true
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
     }
     
-    func saveImageToDirectory(directoryName: DirectoryName, identifier: String, image: UIImage?) {
+    func editImageToDirectory(directoryName: DirectoryName, identifier: String, image: UIImage?) -> Bool {
         
-        let fileURL = createFileURL(directoryName: directoryName, identifier: identifier)
+        let (fileURL, imageName) = createURL(URLCase: .fileURL, directoryName: directoryName, identifier: identifier)
         
         do {
             if let imageData = image?.jpegData(compressionQuality: 0.3) {
                 try imageData.write(to: fileURL)
-                print("Image saved at: \(fileURL)")
+                print("Image edited at: \(fileURL)")
             }
+            return true
         } catch let error {
             print("Failed to save image: \(error.localizedDescription)")
+            return false
         }
     }
     
     func loadImageFromDirectory(directoryName: DirectoryName, with identifier: String) -> UIImage? {
-        let fileURL = createFileURL(directoryName: directoryName, identifier: identifier)
+        let (fileURL, _) = createURL(URLCase: .directory, directoryName: directoryName, identifier: identifier)
         
         guard fileManager.fileExists(atPath: fileURL.path) else { return nil }
         
         return UIImage(contentsOfFile: fileURL.path)
     }
     
-    func removeImageFromDirectory(directoryName: DirectoryName, identifier: String) {
-        let fileURL = createFileURL(directoryName: directoryName, identifier: identifier)
+    func removeImageFromDirectory(directoryName: DirectoryName, identifier: String) -> Bool {
+        let (fileURL, _) = createURL(URLCase: .fileURL, directoryName: directoryName, identifier: identifier)
         
         do {
             if fileManager.fileExists(atPath: fileURL.path) {
                 try fileManager.removeItem(at: fileURL)
             }
+            return true
         } catch let error {
             print("Failed to delete file: \(error.localizedDescription)")
+            return false
         }
     }
 }
