1d817cbf93f56fa9ce4bc29e9cefcf89a37b7999
yeoni <ghj0308@daum.net>
2023-10-08 23:09:54 +0900

    feat: update

--- Orang/Orang/Module/Realm/PetTableRepository.swift
+++ Orang/Orang/Module/Realm/PetTableRepository.swift
@@ -12,6 +12,7 @@ protocol PetTableRepositoryType: AnyObject {
     func fetch() -> Results<PetTable>
     func create(_ item: PetTable)
     func delete(_ item: PetTable)
+    func update(id: ObjectId, _ item: PetTable)
 }
 
 class PetTableRepository: PetTableRepositoryType {
@@ -44,6 +45,29 @@ class PetTableRepository: PetTableRepositoryType {
             
         }
     }
+    
+    func update(id: RealmSwift.ObjectId, _ item: PetTable) {
+        let updateValue: [String: Any] = [
+            "_id": id,
+            "species": item.species,
+            "detailSpecies": item.detailSpecies,
+            "name": item.name,
+            "birthday": item.birthday,
+            "belongDate": item.belongDate,
+            "weight": item.weight,
+            "weightUnit": item.weightUnit,
+            "registrationNum": item.registrationNum,
+            "profileImage": item.profileImage,
+        ]
+        do {
+            try realm.write {
+                realm.create(PetTable.self, value: updateValue, update: .modified)
+            }
+        } catch {
+            print("update Error: \(error)")
+        }
+    }
+    
     
     func loadFileURL() {
         print(realm.configuration.fileURL)
