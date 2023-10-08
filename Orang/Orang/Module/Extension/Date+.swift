cb1c78de1d8698d110c6d520119674904e13fe2b
yeoni <ghj0308@daum.net>
2023-10-08 21:08:21 +0900

    feat: compareToNow / imageLoad용 String 변환 함수 추가

--- Orang/Orang/Module/Extension/Date+.swift
+++ Orang/Orang/Module/Extension/Date+.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Date {
     func toFormattedString() -> String {
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy / MM / dd"
+        formatter.dateFormat = "yyyy년 MM월 dd일"
         
         return formatter.string(from: self)
     }
@@ -24,5 +24,28 @@ extension Date {
     func compareNow() -> ComparisonResult {
         let date = Date()
         return self.compare(date)
+    }
+    
+    func compareToNow() -> Int {
+        
+        let target = self.toFormattedString().toDate()
+        let today = Date().toFormattedString().toDate()
+        
+        //interval 계산
+        let calendar = Calendar.current
+        
+        if let target, let today {
+            let dateGap = calendar.dateComponents([.day], from: target, to: today).day!
+            
+            return dateGap + 1
+        } else {
+            return 0 // TODO: d-day 계산 실패 -> alert 필요?
+        }
+    }
+    
+    func forLoadImage() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH/mm/ss"
+        return formatter.string(from: self)
     }
 }
