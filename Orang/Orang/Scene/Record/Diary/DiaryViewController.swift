b6655370e8d1fd99de0fcb63e05f953459bc0d4a
yeoni <ghj0308@daum.net>
2023-10-10 01:33:51 +0900

    feat: 사진 추가하는 CollectionView 구성

--- Orang/Orang/Scene/Record/Diary/DiaryViewController.swift
+++ Orang/Orang/Scene/Record/Diary/DiaryViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class DiaryViewController: BaseViewController {
     
+    let collectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: setCollectionViewLayout())
+        view.register(PicCollectionViewCell.self, forCellWithReuseIdentifier: PicCollectionViewCell.identifier)
+        view.register(AddCollectionViewCell.self, forCellWithReuseIdentifier: AddCollectionViewCell.identifier)
+        return view
+    }()
+    
     let titleTextField = UnderLineTextField.textFieldBuilder(placeholder: "제목을 입력해 주세요!")
     let contentTextView = UITextView()
     
@@ -32,12 +39,22 @@ class DiaryViewController: BaseViewController {
     
     override func configureHierarchy() {
         super.configureHierarchy()
-        
-        view.addSubview(titleTextField)
-        view.addSubview(contentTextView)
+        [
+            collectionView,
+            titleTextField,
+            contentTextView
+        ]
+            .forEach { view.addSubview($0) }
     }
     
-    override func configureView() {
+    override func setConstraints() {
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(96)
+        }
+        
         titleTextField.snp.makeConstraints { make in
             make.centerY.equalTo(view)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
@@ -50,6 +67,53 @@ class DiaryViewController: BaseViewController {
         }
     }
     
-    override func setConstraints() {
+    override func configureView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+}
+
+extension DiaryViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return 10
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let section = indexPath.section
+        if section == 0 {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AddCollectionViewCell.identifier, for: indexPath) as? AddCollectionViewCell else { return UICollectionViewCell() }
+            
+            return cell
+            
+            
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PicCollectionViewCell.identifier, for: indexPath) as? PicCollectionViewCell else { return UICollectionViewCell() }
+            cell.imageView.backgroundColor = .black
+            cell.completionHandler = {
+                // 해당 imageView 삭제하는 코드 작성
+            }
+            return cell
+        }
+    }
+    
+    static func setCollectionViewLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        let space: CGFloat = 8
+        
+        layout.itemSize = CGSize(width: 80, height: 80)
+        
+        layout.minimumLineSpacing = space
+        layout.minimumInteritemSpacing = space
+        layout.scrollDirection = .horizontal
+        return layout
     }
 }
