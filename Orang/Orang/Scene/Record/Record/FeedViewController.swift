92831cf3a7dec4a30dfc747b0696e5b3a56b0edc
yeoni <ghj0308@daum.net>
2023-10-11 00:54:40 +0900

    feat: 간식 종류와 시간 입력 추가

--- Orang/Orang/Scene/Record/Record/FeedViewController.swift
+++ Orang/Orang/Scene/Record/Record/FeedViewController.swift
@@ -9,19 +9,20 @@ import UIKit
 
 class FeedViewController: BaseViewController {
     
-    let segmentControl = {
-        
-    }
-    let numberTextField = UnderLineTextField.textFieldBuilder(placeholder: "숫자만 적어 주세요!")
-    let unitButton = UIButton.unitPopUpButtonBuilder(menuElement: [
+    let typeTextField = UnderLineTextField.textFieldBuilder(placeholder: "간식 이름을 적어 주세요!")
+    
+    let weightTextField = UnderLineTextField.textFieldBuilder(placeholder: "숫자만 적어 주세요!")
+    let weightUnitButton = UIButton.unitPopUpButtonBuilder(menuElement: [
         UIAction(title: Unit.g.rawValue, handler: { _ in }),
         UIAction(title: Unit.kg.rawValue, handler: { _ in }),
         UIAction(title: Unit.lb.rawValue, handler: { _ in }),
         UIAction(title: "count".localized(), handler: { _ in })
     ])
-    let numberStackView = UIStackView.stackViewBuilder()
+    let weightStackView = UIStackView.stackViewBuilder()
     
-    
+    let dateTextField = UnderLineTextField.textFieldBuilder(placeholder: "날짜를 입력해 주세요!")
+    let timeTextField = UnderLineTextField.textFieldBuilder(placeholder: "시간을 입력해 주세요!")
+    let dateStackView = UIStackView.stackViewBuilder(axis: .horizontal)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,12 +32,12 @@ class FeedViewController: BaseViewController {
     override func setNavigationBar() {
         super.setNavigationBar()
         
-        title = "몸무게 기록 추가하기"
+        title = "간식 기록 추가하기"
         
         let saveButton = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveButtonClicked))
         navigationItem.rightBarButtonItem = saveButton
         
-
+        
     }
     
     @objc func saveButtonClicked() {
@@ -47,20 +48,101 @@ class FeedViewController: BaseViewController {
         super.configureHierarchy()
         
         [
-            numberStackView
+            weightStackView,
+            typeTextField,
+            dateStackView
         ]
             .forEach { view.addSubview($0) }
         
-        numberStackView.AddArrangedSubviews([numberTextField, unitButton])
+        weightStackView.AddArrangedSubviews([weightTextField, weightUnitButton])
+        dateStackView.AddArrangedSubviews([dateTextField, timeTextField])
     }
     
-    override func configureView() {
-        numberStackView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).inset(30)
+    override func setConstraints() {
+        dateStackView.distribution = .fillEqually
+        dateStackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        typeTextField.snp.makeConstraints { make in
+            make.top.equalTo(dateStackView.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        weightStackView.snp.makeConstraints { make in
+            make.top.equalTo(typeTextField.snp.bottom).offset(16)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
     
-    override func setConstraints() {
+    override func configureView() {
+        [dateTextField, timeTextField, typeTextField, weightTextField].forEach { element in
+            element.textAlignment = .center
+            element.delegate = self
+        }
+        dateTextField.tag = 1
+        timeTextField.tag = 2
+        setupDatePicker(textField: dateTextField)
+        setupDatePicker(textField: timeTextField)
+    }
+    
+    
+}
+
+extension FeedViewController {
+    func setupDatePicker(textField: UITextField) {
+        // 여기서 datePicker를 weak로 써줘야 하나?
+        let datePicker = UIDatePicker()
+        datePicker.tag = textField.tag
+        
+        // 원하는 언어로 지역 설정
+        datePicker.locale = Locale(identifier: "ko-KR")
+        datePicker.addTarget(self, action: #selector(dateChange), for: .valueChanged)
+        
+        textField.inputView = datePicker
+        if textField == dateTextField {
+            datePicker.preferredDatePickerStyle = .inline
+            datePicker.datePickerMode = .date
+            dateTextField.text = datePicker.date.toFormattedString()
+        } else {
+            datePicker.datePickerMode = .time
+            datePicker.preferredDatePickerStyle = .wheels
+            timeTextField.text = datePicker.date.toFormattedStringTime()
+        }
+    }
+    
+    // 값이 변할 때 마다 동작
+    @objc func dateChange(_ sender: UIDatePicker) {
+        if sender.tag == 1 {
+            //            birth = sender.date
+            dateTextField.text = sender.date.toFormattedString()
+            
+        } else {
+            //            meetDate = sender.date
+            timeTextField.text = sender.date.toFormattedStringTime()
+        }
+    }
+}
+
+
+extension FeedViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == dateTextField || textField == timeTextField {
+            return false
+        } else if textField == weightTextField {
+            let isNumber = CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: string))
+            let withDecimal = (
+                string == NumberFormatter().decimalSeparator &&
+                textField.text?.contains(string) == false
+            )
+            return isNumber || withDecimal
+        } else {
+            return true
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
     }
 }
