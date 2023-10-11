d92bb1b2e830b0590c4f18c9d2fe0f06f5cdb31a
yeoni <ghj0308@daum.net>
2023-10-11 21:12:02 +0900

    feat: TextViewBuilder 추가

---
+++ Orang/Orang/Scene/Base/Builder/TextViewBuilder.swift
@@ -0,0 +1,22 @@
+//
+//  TextViewBuilder.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/11.
+//
+
+import UIKit
+
+extension UITextView {
+    static func TextViewBuilder() -> UITextView {
+        let view = UITextView()
+        view.layer.cornerRadius = 16
+        view.layer.borderColor = Design.Color.border.cgColor
+        view.font = .systemFont(ofSize: 14)
+        view.textContainerInset = UIEdgeInsets(top: 16, left: 8, bottom: 16, right: 8)
+        view.layer.borderWidth = 1
+        view.backgroundColor = Design.Color.background
+        view.tintColor = Design.Color.tintColor
+        return view
+    }
+}
