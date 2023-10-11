0978afd67d764aa6ab1b0c6d0c0681e95cffd351
yeoni <ghj0308@daum.net>
2023-10-11 18:28:48 +0900

    edit: parameter default 값 추가

--- Orang/Orang/Scene/Base/Builder/LabelBuilder.swift
+++ Orang/Orang/Scene/Base/Builder/LabelBuilder.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UILabel {
-    static func labelBuilder(text: String = "", size: CGFloat, weight: UIFont.Weight, color: UIColor? = Design.Color.content, alignment: NSTextAlignment = .left, settingTitle: Bool) -> UILabel {
+    static func labelBuilder(text: String = "", size: CGFloat, weight: UIFont.Weight, color: UIColor? = Design.Color.content, alignment: NSTextAlignment = .left, settingTitle: Bool = false) -> UILabel {
         let view = UILabel()
         if settingTitle {
             view.snp.makeConstraints { make in
