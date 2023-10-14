cafdaf6cc79a1e215c44c4b3ed168f013ab57d2e
yeoni <ghj0308@daum.net>
2023-10-14 15:51:12 +0900

    feat: CheckRecord
    기존의 AbnormalSymptoms를 제네릭으로 바꾸어 활용도가 높도록 바꿈

--- Orang/Orang/Module/Data/RecordType/CheckRecord/CheckRecord.swift
+++ Orang/Orang/Module/Data/RecordType/CheckRecord/CheckRecord.swift
@@ -1,19 +1,19 @@
 //
-//  AbnormalSymptoms.swift
+//  CheckRecord.swift
 //  Orang
 //
-//  Created by yeoni on 2023/10/11.
+//  Created by yeoni on 2023/10/14.
 //
 
 import Foundation
 
-struct AbnormalSymptoms {
-    var type: AbnormalSymptomsType
+struct CheckRecord<T: CheckProtocol> {
+    var type: T
     var title: String
     var subtitle: String
     var ischecked: Bool
     
-    init(type: AbnormalSymptomsType) {
+    init(type: T) {
         self.type = type
         self.title = type.title
         self.subtitle = type.subtitle
---
+++ Orang/Orang/Module/Protocol/CheckProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  CheckProtocol.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/14.
+//
+
+import Foundation
+
+protocol CheckProtocol {
+    var title: String { get }
+    var subtitle: String { get }
+}
