ab80ff869787f2fb63b6d61d4bacf6253129576b
yeoni <ghj0308@daum.net>
2023-10-14 15:03:40 +0900

    feat: poo / peeVC 구성할 collectionViewCell 생성

---
+++ Orang/Orang/Scene/Record/WithViewController/Record/PooPee/PooPeeCollectionViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  PooPeeCollectionViewCell.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/13.
+//
+
+import UIKit
+import M13Checkbox
+
+final class PooPeeCollectionViewCell: BaseCollectionViewCell {
+    
+    let titleLabel = UILabel.labelBuilder(text: "타이틀입니당", size: 16, weight: .bold)
+    let subtitleLabel = UILabel.labelBuilder(text: "서브타이틀입니당", size: 14, weight: .regular)
+    
+    let checkbox = {
+        let button = M13Checkbox(frame: .zero)
+        button.stateChangeAnimation = .bounce(.fill) // orFlat
+        button.markType = .checkmark
+        button.checkState = .unchecked
+        button.tintColor = Design.Color.tintColor
+        return button
+    }()
+    
+    let labelStackView = UIStackView.stackViewBuilder(space: 2, axis: .vertical)
+    
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        
+        self.addSubview(labelStackView)
+        self.addSubview(checkbox)
+        
+        labelStackView.AddArrangedSubviews([titleLabel, subtitleLabel])
+    }
+    
+    override func setConstraints() {
+        labelStackView.distribution = .fillProportionally
+        
+        labelStackView.snp.makeConstraints { make in
+            make.centerY.equalTo(self.safeAreaLayoutGuide)
+            make.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).inset(30)
+            make.trailing.equalTo(checkbox.snp.leading).offset(10)
+        }
+        
+        checkbox.snp.makeConstraints { make in
+            make.centerY.equalTo(self.safeAreaLayoutGuide)
+            make.height.equalTo(checkbox.snp.width)
+            make.width.equalTo(25)
+            make.trailing.equalTo(self.safeAreaLayoutGuide).inset(30)
+        }
+    }
+    
+    override func configureView() {
+//        guard let symptom else { return }
+//        if symptom.ischecked {
+//            checkbox.setCheckState(.checked, animated: true)
+//        } else {
+//            checkbox.setCheckState(.unchecked, animated: true)
+//        }
+//        titleLabel.text = symptom.title
+//        subtitleLabel.text = symptom.subtitle
+    }
+}
