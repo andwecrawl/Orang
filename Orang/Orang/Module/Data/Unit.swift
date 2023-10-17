758a602d9e423df0525172ae6bb555297ec622c1
yeoni <ghj0308@daum.net>
2023-10-17 15:14:47 +0900

    edit: Unit toString 변수 추가

--- Orang/Orang/Module/Data/Unit.swift
+++ Orang/Orang/Module/Data/Unit.swift
@@ -9,5 +9,18 @@ import Foundation
 import RealmSwift
 
 enum Unit: String, PersistableEnum {
-    case g, kg, lb
+    case g, kg, lb, count
+    
+    var toString: String {
+        switch self {
+        case .g:
+            return "g"
+        case .kg:
+            return "kg"
+        case .lb:
+            return "lb"
+        case .count:
+            return "count".localized()
+        }
+    }
 }
