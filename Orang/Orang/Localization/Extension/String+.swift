1e9d75ec5ace0e7c110ce8a8a4d212f1310ee28d
yeoni <ghj0308@daum.net>
2023-10-17 11:37:16 +0900

    feat: toDateContainsTime

--- Orang/Orang/Localization/Extension/String+.swift
+++ Orang/Orang/Localization/Extension/String+.swift
@@ -15,6 +15,13 @@ extension String {
         
         return formatter.date(from: self)
     }
+    
+    func toDateContainsTime() -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 dd일 HH시 mm분"
+        
+        return formatter.date(from: self)
+    }
 }
 
 
