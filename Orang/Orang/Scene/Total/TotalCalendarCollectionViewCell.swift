83ee3e493b6743cd8d9f9f510c8c768d59336f9c
yeoni <ghj0308@daum.net>
2023-10-18 21:28:36 +0900

    feat: TotalCalendarCollectionViewCell

---
+++ Orang/Orang/Scene/Total/TotalCalendarCollectionViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TotalCalendarCollectionViewCell.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/17.
+//
+
+import UIKit
+
+class TotalCalendarCollectionViewCell: BaseCollectionViewCell {
+    
+    
+    
+    
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+    
+}
