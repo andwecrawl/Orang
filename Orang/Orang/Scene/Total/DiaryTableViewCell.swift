d4228f2645bdba378545dee00010e0dc4e2d91ab
yeoni <ghj0308@daum.net>
2023-10-20 01:14:13 +0900

    feat: DiaryCollectionViewCell

---
+++ Orang/Orang/Scene/Total/DiaryTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  DiaryTableViewCell.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/19.
+//
+
+import UIKit
+
+class DiaryTableViewCell: BaseTableViewCell {
+
+    let diaryImageView = UIImageView.imageViewBuilder(size: 70)
+    let titleLabel = UILabel.labelBuilder(text: "제목입니당", size: 16, weight: .semibold, alignment: .left)
+    let subtitleLabel = UILabel.labelBuilder(text: "서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당", size: 14, weight: .regular, alignment: .justified)
+    
+    let textStackView = UIStackView.stackViewBuilder(space: 2, axis: .vertical)
+    
+    let arrowImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "greaterthan")
+        view.tintColor = .lightGray
+        return view
+    }()
+    
+    override func configureHierarchy() {
+        [
+            diaryImageView,
+            textStackView,
+            arrowImageView
+        ]
+            .forEach { self.addSubview($0) }
+        
+        textStackView.addArrangedSubviews([titleLabel, subtitleLabel])
+    }
+    
+    override func setConstraints() {
+        diaryImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(12)
+        }
+        
+        textStackView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(diaryImageView.snp.trailing).offset(12)
+            make.trailing.equalToSuperview().inset(36)
+        }
+        
+        arrowImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(8)
+            make.width.equalTo(10)
+            make.height.equalTo(20)
+        }
+    }
+
+    override func configureView() {
+        textStackView.distribution = .equalSpacing
+        
+        diaryImageView.layer.cornerRadius = 0
+        diaryImageView.backgroundColor = .gray
+        titleLabel.numberOfLines = 1
+        subtitleLabel.numberOfLines = 2
+    }
+    
+}
