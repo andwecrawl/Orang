bb8d5943095bd192936eeded38f1549e3651f880
yeoni <ghj0308@daum.net>
2023-10-19 21:25:05 +0900

    feat: CalendarView

---
+++ Orang/Orang/Scene/Total/CalendarView.swift
@@ -0,0 +1,87 @@
+//
+//  CalendarView.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/19.
+//
+
+import UIKit
+import FSCalendar
+
+class CalendarView: BaseView {
+    
+    let innerView = UIView()
+    lazy var outerView = {
+        let view = UIView.shadowViewBuilder(innerView: innerView)
+        return view
+    }()
+    
+    let calendarLabel = UILabel.labelBuilder(text: "YYYY년 MM월", size: 18, weight: .bold, alignment: .left)
+    
+    private lazy var previousButton : UIButton = {
+        let button = UIButton()
+        let config = UIImage.SymbolConfiguration(weight: .semibold)
+        let image = UIImage(systemName: "chevron.left", withConfiguration: config)
+        button.setImage(image, for: .normal)
+        return button
+    }()
+    
+    private lazy var nextButton : UIButton = {
+        let button = UIButton()
+        let config = UIImage.SymbolConfiguration(weight: .semibold)
+        let image = UIImage(systemName: "chevron.right", withConfiguration: config)
+        button.setImage(image, for: .normal)
+        return button
+    }()
+    
+    let monthlyStackView = UIStackView.stackViewBuilder(space: 8, axis: .horizontal)
+    
+    lazy var calendar = FSCalendar()
+    
+    
+    override func configureHierarchy() {
+        
+        self.addSubview(outerView)
+        UIView.addOuterShadowAndRadius(outerView: outerView, innerView: innerView)
+        
+        [
+            monthlyStackView,
+            calendar
+        ]
+            .forEach { innerView.addSubview($0) }
+        
+        monthlyStackView.addArrangedSubviews([calendarLabel, previousButton, nextButton])
+    }
+    
+    
+    override func setConstraints() {
+        outerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        monthlyStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(16)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(40)
+        }
+        
+        [previousButton, nextButton].forEach { button in
+            button.snp.makeConstraints { make in
+                make.width.equalTo(button.snp.height).multipliedBy(0.8)
+            }
+        }
+        
+        calendarLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+        }
+        
+        calendar.snp.makeConstraints { make in
+            make.top.equalTo(monthlyStackView.snp.bottom).offset(8)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(16)
+        }
+    }
+    
+    
+    
+}
