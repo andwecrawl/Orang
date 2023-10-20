ac5b8df574d8e5b7877b28b1fc2c81c267b3e651
yeoni <ghj0308@daum.net>
2023-10-20 17:00:38 +0900

    feat: SettingViewController

---
+++ Orang/Orang/Scene/Profile/SettingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingViewController.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/20.
+//
+
+import UIKit
+
+class SettingViewController: BaseViewController {
+    
+    
+    
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+    
+}
