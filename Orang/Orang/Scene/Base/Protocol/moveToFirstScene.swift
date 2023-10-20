b140bba79ae2f65ab20164aa6197d813631d6903
yeoni <ghj0308@daum.net>
2023-10-20 20:00:59 +0900

    edit: 모아보기 VC 추가

--- Orang/Orang/Scene/Base/Protocol/moveToFirstScene.swift
+++ Orang/Orang/Scene/Base/Protocol/moveToFirstScene.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Toast
 
 protocol MoveToFirstScene {
     func moveToFirstScene()
@@ -19,16 +20,19 @@ extension MoveToFirstScene {
         
         let tabBar = UITabBarController()
         
+        let totalNav = UINavigationController(rootViewController: TotalViewController())
+        totalNav.tabBarItem = UITabBarItem(title: "모아보기", image: UIImage(systemName: "house.fill"), tag: 0)
+        
         let alertNav = UINavigationController(rootViewController: AlertViewController())
         alertNav.tabBarItem = UITabBarItem(title: "일정 관리", image: UIImage(systemName: "alarm"), tag: 0)
         
         let recordNav = UINavigationController(rootViewController: RecordViewController())
-        recordNav.tabBarItem = UITabBarItem(title: "기록하기", image: UIImage(systemName: "pencil.and.outline"), tag: 0)
+        recordNav.tabBarItem = UITabBarItem(title: "기록하기", image: UIImage(systemName: "pencil.and.outline"), tag: 1)
         
         let profileNav = UINavigationController(rootViewController: ProfileViewController())
-        profileNav.tabBarItem = UITabBarItem(title: "아이 설정", image: UIImage(systemName: "pawprint.circle.fill"), tag: 0)
+        profileNav.tabBarItem = UITabBarItem(title: "아이 설정", image: UIImage(systemName: "pawprint.circle.fill"), tag: 2)
         
-        tabBar.setViewControllers([alertNav, recordNav, profileNav], animated: true)
+        tabBar.setViewControllers([totalNav, alertNav, recordNav, profileNav], animated: true)
         
         SceneDelegate?.window?.rootViewController = tabBar
         SceneDelegate?.window?.makeKeyAndVisible()
