6445940f1ccf9201669050d3302f5c09ffac97ea
yeoni <ghj0308@daum.net>
2023-10-20 20:00:06 +0900

    edit: bg 색깔 수정

--- Orang/Orang/Scene/Total/Cell/DiaryTableViewCell.swift
+++ Orang/Orang/Scene/Total/Cell/DiaryTableViewCell.swift
@@ -23,6 +23,8 @@ class DiaryTableViewCell: BaseTableViewCell {
     }()
     
     override func configureHierarchy() {
+        self.backgroundColor = .clear
+        
         [
             diaryImageView,
             textStackView,
