18e63a859d42700fdf2995b0f1477c091236126a
yeoni <ghj0308@daum.net>
2023-10-22 03:16:59 +0900

    feat: DailyTempTableViewCell
    임시로 프로필 보여주는 뷰 추가

---
+++ Orang/Orang/Scene/Total/Cell/DailyTempTableViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  DailyTempTableViewCell.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/20.
+//
+
+import UIKit
+
+class DailyTempTableViewCell: BaseTableViewCell {
+
+    let diaryImageView = UIImageView.imageViewBuilder(size: 60)
+    let titleLabel = UILabel.labelBuilder(text: "제목입니당", size: 15, weight: .semibold, alignment: .left)
+    let subtitleLabel = UILabel.labelBuilder(text: "서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당서브타이틀입니당", size: 14, weight: .regular, alignment: .justified)
+    
+    let textStackView = UIStackView.stackViewBuilder(space: 2, axis: .vertical)
+    
+    let arrowImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "greaterthan")
+        view.tintColor = Design.Color.tintColor
+        return view
+    }()
+    
+    override func configureHierarchy() {
+        self.backgroundColor = .clear
+        
+        [
+            diaryImageView,
+            textStackView,
+            arrowImageView
+        ]
+            .forEach { self.addSubview($0) }
+        
+        textStackView.addArrangedSubviews([titleLabel, subtitleLabel])
+    }
+    
+    override func setConstraints() {
+        diaryImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(12)
+        }
+        
+        textStackView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(diaryImageView.snp.trailing).offset(12)
+            make.trailing.equalToSuperview().inset(36)
+        }
+        
+        arrowImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(8)
+            make.width.equalTo(10)
+            make.height.equalTo(20)
+        }
+    }
+
+    override func configureView() {
+        textStackView.distribution = .equalSpacing
+        
+        diaryImageView.backgroundColor = .gray
+        titleLabel.numberOfLines = 1
+        subtitleLabel.numberOfLines = 2
+    }
+    
+}
