efc24b53e4c234f40dce9a2ab1c19e7fcc120ce6
yeoni <ghj0308@daum.net>
2023-10-22 21:19:55 +0900

    feat: 이미지 변수 추가
    추후 이미지 등록 시 수정 필요

--- Orang/Orang/Module/Design/Design.swift
+++ Orang/Orang/Module/Design/Design.swift
@@ -9,7 +9,21 @@ import UIKit
 
 class Design {
     enum image {
+        // 자잘한 이미지들
         static let poo = "poo"
+        
+        static let diary = "lizard.fill"
+        static let weight = "lizard.fill"
+        static let snack = "lizard.fill"
+        static let peePoo = "lizard.fill"
+        static let abnormalSymptoms = "lizard.fill"
+        static let vaccine = "lizard.fill"
+        static let medicalHistory = "lizard.fill"
+        
+        static let totalVC = ""
+        static let alertVC = ""
+        static let recordVC = ""
+        static let profileVC = ""
     }
     
     enum Color {
