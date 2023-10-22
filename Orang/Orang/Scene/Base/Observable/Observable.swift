9ecaf9eb94bbce8756bd4e0d8ca18e13e13c2662
yeoni <ghj0308@daum.net>
2023-10-23 06:09:33 +0900

    feat: Observable

---
+++ Orang/Orang/Scene/Base/Observable/Observable.swift
@@ -0,0 +1,29 @@
+//
+//  Observable.swift
+//  Orang
+//
+//  Created by yeoni on 2023/10/23.
+//
+
+import Foundation
+
+class Observable<T> {
+    
+    var listener: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ closure: @escaping ((T) -> Void)) {
+        print(#function)
+        closure(value)
+        listener = closure
+    }
+}
