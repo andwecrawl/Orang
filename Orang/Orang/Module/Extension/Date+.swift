0a8a8f3893c96aa463e95691f2f55031cf8ed912
yeoni <ghj0308@daum.net>
2023-10-24 00:54:21 +0900

    feat: 편한 비교를 위한 date 변수 추가

--- Orang/Orang/Module/Extension/Date+.swift
+++ Orang/Orang/Module/Extension/Date+.swift
@@ -8,6 +8,29 @@
 import Foundation
 
 extension Date {
+    
+    var startDateOfMonth: Date {
+        guard let date = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: self)) else {
+            fatalError("Unable to get start date from date")
+        }
+        return date
+    }
+
+    var endDateOfMonth: Date {
+        guard let date = Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startDateOfMonth) else {
+            fatalError("Unable to get end date from date")
+        }
+        return date
+    }
+
+    var startOfTheDate: Date {
+        return Calendar.current.startOfDay(for: self)
+    }
+    
+    var nextDayOfTheDate: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: startOfTheDate)!
+    }
+    
     func toCalendarTitle() -> String {
         let formatter = DateFormatter()
         if Locale.current == Locale(identifier: "ko_KR") {
