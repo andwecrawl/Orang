b7cd2c8bc88f4fff72fa0cb0aee76346fbd991f2
yeoni <ghj0308@daum.net>
2023-10-26 00:12:50 +0900

    edit: 시작 화면 변경

--- Orang/Orang/Application/SceneDelegate.swift
+++ Orang/Orang/Application/SceneDelegate.swift
@@ -8,63 +8,48 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         // View 시작
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         
-        let tabBar = UITabBarController()
-        
-        let totalNav = UINavigationController(rootViewController: TotalViewController())
-        totalNav.tabBarItem = UITabBarItem(title: "totalNavigationTitle".localized(), image: Design.image.totalVC, tag: 0)
-        
-        let alertNav = UINavigationController(rootViewController: AlertViewController())
-        alertNav.tabBarItem = UITabBarItem(title: "AlertNavigationTitle".localized(), image: Design.image.alertVC, tag: 0)
-        
-        let recordNav = UINavigationController(rootViewController: RecordViewController())
-        recordNav.tabBarItem = UITabBarItem(title: "recordNavigationTitle".localized(), image: Design.image.recordVC, tag: 1)
-        
-        let profileNav = UINavigationController(rootViewController: ProfileViewController())
-        profileNav.tabBarItem = UITabBarItem(title: "ProfileNavigationTitle".localized(), image: Design.image.profileVC, tag: 2)
-        
-        tabBar.setViewControllers([totalNav, alertNav, recordNav, profileNav], animated: true)
-        window?.rootViewController = tabBar
+        let vc = LaunchViewController()
+        window?.rootViewController = vc
         window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    
 }
 
