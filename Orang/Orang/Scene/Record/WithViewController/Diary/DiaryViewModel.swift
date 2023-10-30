f82afb2abd3ae44cd146996c3b3de7f90e635ee2
yeoni <ghj0308@daum.net>
2023-10-31 01:09:51 +0900

    feat: DiaryViewModel Update

--- Orang/Orang/Scene/Record/WithViewController/Diary/DiaryViewModel.swift
+++ Orang/Orang/Scene/Record/WithViewController/Diary/DiaryViewModel.swift
@@ -6,13 +6,24 @@
 //
 
 import Foundation
+import RealmSwift
 
 class DiaryViewModel {
+    
+    let repository = PetTableRepository()
     
     var title: Observable<String?> = Observable("")
     var content: Observable<String?> = Observable("")
     var imageCount: Observable<Int> = Observable(0)
-    var isValid = Observable(false)
+    var record: RecordTable?
+    
+    var add: Int {
+        return imageCount.value >= 5 ? 0 : 1
+    }
+    
+    var images: Int {
+        return imageCount.value < 6 ? imageCount.value : 5
+    }
     
     var numberOfItems: Int {
         let add = imageCount.value >= 5 ? 0 : 1
@@ -20,7 +31,33 @@ class DiaryViewModel {
         return add + images
     }
     
-    func checkValidation(completionHandler: @escaping () -> ()) {
-        completionHandler()
+    func checkValidation(petID: ObjectId, titleHandler: @escaping (String) -> (), imageHandler: @escaping (String) -> ()) -> Date? {
+        guard let title = title.value else {
+            titleHandler("noTitleError".localized())
+            return nil
+        }
+        
+        if title.isEmpty {
+            titleHandler("noTitleError".localized())
+            return nil
+        }
+        
+        if imageCount.value <= 0 {
+            imageHandler("noImageError".localized())
+        }
+        
+        let content = content.value
+        let date = Date()
+        
+        let record = RecordTable(recordType: .diary, petID: petID, recordDate: date, title: title, content: content, images: [])
+        self.record = record
+        return record.createdDate
+    }
+    
+    func saveData(images: [String]) {
+        if let record {
+            record.imageArray = images
+            repository.updateRecords(id: record.petId, record)
+        }
     }
 }
