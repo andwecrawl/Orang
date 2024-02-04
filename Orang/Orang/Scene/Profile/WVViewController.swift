d18274bafad3c8e95c5635ee1ced3aade376e5b9
yeoni <ghj0308@daum.net>
2024-02-04 23:52:38 +0900

    feat: WVViewController
    WebViewController

---
+++ Orang/Orang/Scene/Profile/WVViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WVViewController.swift
+//  Orang
+//
+//  Created by yeoni on 2/13/24.
+//
+
+import Foundation
+
+class WVViewController: BaseViewController {
+    
+    var settingType: Setting?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+    
+    
+    
+}
