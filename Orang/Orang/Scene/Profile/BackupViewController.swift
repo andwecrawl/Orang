d70f5b57f13b10c9570c571ddbed9334b330eb5f
yeoni <ghj0308@daum.net>
2024-02-05 23:40:32 +0900

    feat: BackupViewController

---
+++ Orang/Orang/Scene/Profile/BackupViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BackupViewController.swift
+//  Orang
+//
+//  Created by yeoni on 2/13/24.
+//
+
+import Foundation
+
+class BackupViewController: BaseViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+    
+    
+    
+}
