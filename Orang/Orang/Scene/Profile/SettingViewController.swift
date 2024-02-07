7fd0cc5f2bf33155b48d4e15be5cce5ae6691496
yeoni <ghj0308@daum.net>
2024-02-07 23:20:22 +0900

    feat: tableViewSetting

--- Orang/Orang/Scene/Profile/SettingViewController.swift
+++ Orang/Orang/Scene/Profile/SettingViewController.swift
@@ -7,21 +7,82 @@
 
 import UIKit
 
+enum Setting: String, CaseIterable {
+//    case alertSettings
+    case backup = "기록 백업"
+    case bugReport = "버그, 오류 제보"
+    case emailToDeveloper = "개발자에게 문의"
+    case information = "개인정보 처리방침"
+}
+
 class SettingViewController: BaseViewController {
     
+    private lazy var tableView = {
+        let view = UITableView(frame: .zero, style: .insetGrouped)
+        view.dataSource = self
+        view.delegate = self
+        return view
+    }()
+    
+    let settingList = Setting.allCases
     
     
     
-    override func configureHierarchy() {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
     }
     
-    override func setConstraints() {
+    
+    override func configureHierarchy() {
+        super.configureHierarchy()
         
+        view.addSubview(tableView)
+    }
+    
+    override func setConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     override func configureView() {
         
     }
     
+}
+
+
+// tableview Setting
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = settingList[indexPath.row].rawValue
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = indexPath.row
+        
+        var vc: UIViewController
+        let settingType = settingList[row]
+        
+        switch settingType {
+            
+        case .backup:
+            vc = BackupViewController()
+            
+        default:
+            vc = WVViewController()
+            guard let vc = vc as? WVViewController else { return }
+            vc.settingType = settingType
+            
+        }
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
